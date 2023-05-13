c1395e6aedb726b04f6562acb89d790555428467
gauravghost <gyanendrak874@gmail.com>
2023-05-13 06:50:34 +0530

    Added Foreign key in airport for city

---
+++ src/migrations/20230512233929-update-city-airport-association.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addConstraint('airports', {
+      type: 'FOREIGN KEY',
+      name: 'city_fkey_constraint',
+      fields: ['cityId'],
+      references: {
+        table: 'Cities',
+        field: 'id'
+      },
+      onUpdate: 'cascade',
+      onDelete: 'cascade'
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint('Airports', 'city_fkey_constraint')
+  }
+};
--- src/models/airport.js
+++ src/models/airport.js
@@ -11,6 +11,11 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsTo(models.City, {
+        foreignkey: 'cityId',
+        onDetede: 'cascade',
+        onUpdate: 'cascade'
+      });
     }
   }
   Airport.init({
