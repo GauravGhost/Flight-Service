5d0723ab69d8d6d817b61df03a371b0f4e33f9dd
gauravghost <gyanendrak874@gmail.com>
2023-05-23 10:48:07 +0530

    FEAT: Added filters for get all flights

--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -5,10 +5,14 @@ const { FlightController } = require('../../controllers');
 const { FlightMiddlewares } = require('../../middleware');
 
 
-// /api/v1/airports POST
+// /api/v1/flights POST
 router.post('/',
-        FlightMiddlewares.validateCreateRequest,
-        FlightController.createFlight);
+    FlightMiddlewares.validateCreateRequest,
+    FlightController.createFlight);
+
+// /api/v1/flights?trip=MUM-DEL GET
+router.get('/',
+    FlightController.getAllFlights);
 
 
 module.exports = router;
